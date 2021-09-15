2cdf7fe76e0a723a4981f10dd4b43773f50cc339
Johan van den Dorpe <johan@vdltech.net>
2021-09-15 11:47:02 +0200

    Ensure native-vlan is set correctly on trunk ports

--- napalm_netiron/netiron.py
+++ napalm_netiron/netiron.py
@@ -1164,14 +1164,12 @@ class NetIronDriver(NetworkDriver):
             )
 
             for port in access_ports:
-                result[port]['access-vlan'] = vlan['vlan']
+                if int(vlan['vlan']) <= 4094:
+                    result[port]['access-vlan'] = vlan['vlan']
 
             for port in trunk_ports:
-                result[port].update({
-                    'tagged-native-vlan': True,
-                    'native-vlan': 1
-                })
-                result[port]['trunk-vlans'].append(vlan['vlan'])
+                if int(vlan['vlan']) <= 4094:
+                    result[port]['trunk-vlans'].append(vlan['vlan'])
 
         # Add ports with VLANs from VLLs
         if not self.show_mpls_config:
@@ -1184,6 +1182,12 @@ class NetIronDriver(NetworkDriver):
             # Ignore VLLs with no interface
             if interface:
                 result[interface]['access-vlan'] = vll['vlan']
+
+        # Set native vlan for tagged ports
+        for port, data in result.items():
+            if data['trunk-vlans'] and data['access-vlan']:
+                result[port]['native-vlan'] = data['access-vlan']
+                result[port]['access-vlan'] = -1
 
         return result
 
